3744e2e69f1a116ab9819fe3f8e93f2ad19bac37
jruiz002 <josegrg04@gmail.com>
2024-08-30 22:16:48 -0600

    Se agregó la vista de login

---
+++ app/src/main/java/com/joseruiz/suprstudent/screens/Login.kt
@@ -0,0 +1,144 @@
+package com.joseruiz.suprstudent.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.joseruiz.suprstudent.R
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.core.content.ContextCompat
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreen() {
+    val context = LocalContext.current
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 32.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Iniciar sesión",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = { /* Acción para cambiar el email */ },
+                label = { Text("Email Address") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White),
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.user),
+                        contentDescription = "Email Icon"
+                    )
+                }
+
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = "",
+                onValueChange = { /* Acción para cambiar la contraseña */ },
+                label = { Text("Password") },
+                visualTransformation = PasswordVisualTransformation(),
+                modifier = Modifier
+                    .fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        painter = painterResource(id = R.drawable.lock),
+                        contentDescription = "Password Icon"
+                    )
+                }
+            )
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "¿No tienes una cuenta? Créala",
+                    modifier = Modifier
+                        .clickable { /* Acción de Logearse */ }
+                        .weight(1f),
+                    textAlign = TextAlign.Center,
+                    fontSize = 16.sp,
+                    style = TextStyle(
+                        fontStyle = FontStyle.Italic // Aplicar cursiva
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Button(
+                onClick = { /* Acción para iniciar sesión */ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                shape = RoundedCornerShape(8.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(ContextCompat.getColor(context, R.color.customMaroon)))
+            ) {
+                Text(text = "Login", color = Color.White)
+            }
+
+            Spacer(modifier = Modifier.height(70.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "Logo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(100.dp)
+            )
+        }
+
+        // Footer
+        Image(
+            painter = painterResource(id = R.drawable.footer),
+            contentDescription = "Footer",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth()
+                .height(350.dp)
+        )
+    }
+}
