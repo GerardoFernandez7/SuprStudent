bde10035290f84a4494b7504aa38bac472b112c7
GerardoFernandez7 <101151869+GerardoFernandez7@users.noreply.github.com>
2024-10-01 08:39:39 -0600

    Se agrego un filtro en la vista ejercicio y se modifico la vista academica

--- app/src/main/java/com/joseruiz/suprstudent/screens/Exercise.kt
+++ app/src/main/java/com/joseruiz/suprstudent/screens/Exercise.kt
@@ -206,7 +206,7 @@ fun ExerciseFilterView(
         // Dropdown para músculo
         DropdownFilterMenu(
             label = "Muscle",
-            options = listOf("abdominals", "biceps", "chest", "glutes", "quadriceps", "triceps", "forearms", "calves"),
+            options = listOf("abdominals", "biceps", "chest", "glutes", "quadriceps", "triceps", "forearms", "calves", "legs"),
             selectedOption = selectedMuscle,
             onOptionSelected = { option ->
                 selectedMuscle = option
